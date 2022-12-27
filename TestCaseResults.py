dab08f720565a60e9f7dd097d6e7e2d0b32d6c6e
Cruz, Alejandra <Alejandra.Cruz@osii.com>
2022-12-27 13:14:21 -0600

    Updated pipeline to pass testcycle variable from paramaters

--- TestCaseResults.py
+++ TestCaseResults.py
@@ -4,7 +4,6 @@ from py_jama_rest_client.client import JamaClient
 testname = str(sys.argv[0]) #uses the name entered by robotframework
 testcyle = str(sys.argv[1])
 oauth_client = JamaClient(host_domain='https://osi.jamacloud.com', credentials=('r6ft8sksuttv8sp', 'j8pvfl0t7b64xv55rohy8dksy'), oauth=True)
-#testcyle= 9373533#this needs to change every testcycle
 testruns = oauth_client.get_testruns(testcyle)
 
 for run in testruns:
